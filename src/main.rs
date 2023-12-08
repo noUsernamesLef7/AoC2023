a8bc4c9a4689cdd9105c53e343a3057d41505631
Jason Brown <jason@jasonrbrown.dev>
2023-12-07 23:38:53 -0700

    Refactor Day 7, Part 1

--- src/main.rs
+++ src/main.rs
@@ -187,70 +187,79 @@ fn day7(input: String) {
                 .then(self.card5.cmp(&other.card5))
         }
     }
+    impl Hand{
+        fn new(characters: &str, bid: usize) -> Hand {
+            let cards: Vec<Face> = characters.chars()
+                .map(|character| match character {
+                    'A' => Face::Ace,
+                    'K' => Face::King,
+                    'Q' => Face::Queen,
+                    'J' => Face::Jack,
+                    'T' => Face::Ten,
+                    '9' => Face::Nine,
+                    '8' => Face::Eight,
+                    '7' => Face::Seven,
+                    '6' => Face::Six,
+                    '5' => Face::Five,
+                    '4' => Face::Four,
+                    '3' => Face::Three,
+                    '2' => Face::Two,
+                    _ => panic!("The character {} does not match any known card!", character),
+                })
+                .collect();
+
+            Hand {
+                card1: cards[0],
+                card2: cards[1],
+                card3: cards[2],
+                card4: cards[3],
+                card5: cards[4],
+                bid:bid,
+                score: Self::calculate_score(cards),
+            }
+        }
+
+        fn calculate_score(cards: Vec<Face>) -> ScoreType {
+            // Get a HashMap containing the number of each type of card in the hand
+            let counts = cards.into_iter().counts();
+
+            match counts.values().max().unwrap() {
+                5 => ScoreType::FiveOfAKind,
+                4 => ScoreType::FourOfAKind,
+                3 => match counts.values().find(|&&count| count == 2) {
+                    Some(_i) => ScoreType::FullHouse,
+                    None => ScoreType::ThreeOfAKind,
+                },
+                2 => match counts.values().duplicates().find(|&&count| count == 2) {
+                    Some(_i) => ScoreType::TwoPair,
+                    None => ScoreType::OnePair
+                },
+                1 => ScoreType::HighCard,
+                _ => panic!("{:?} is not a valid value to compute score on!", counts.values()),
+            }
+        }
+    }
 
     // Turn input into a Vec<Hand>
     let mut hands = Vec::new();
     for line in input.lines() {
+
+        // Break input line into a list of chars and the bid
         let mut raw_data = line.split_whitespace();
-
-        // map the chars for each card into Face enum
-        let faces: Vec<Face> = raw_data.next()
+        let faces = raw_data.next()
+            .unwrap();
+        let bid: usize = raw_data.next()
             .unwrap()
-            .chars()
-            .map(|character| match character {
-                'A' => Face::Ace,
-                'K' => Face::King,
-                'Q' => Face::Queen,
-                'J' => Face::Jack,
-                'T' => Face::Ten,
-                '9' => Face::Nine,
-                '8' => Face::Eight,
-                '7' => Face::Seven,
-                '6' => Face::Six,
-                '5' => Face::Five,
-                '4' => Face::Four,
-                '3' => Face::Three,
-                '2' => Face::Two,
-                _ => panic!("The character {} does not match any known card!", character),
-            })
-            .collect();
-
-        // Figure out ScoreType
-        let counts = faces.clone()
-            .into_iter()
-            .counts();
-        let score = match counts.values().max().unwrap() {
-            5 => ScoreType::FiveOfAKind,
-            4 => ScoreType::FourOfAKind,
-            3 => match counts.values().find(|&&count| count == 2) {
-                Some(_i) => ScoreType::FullHouse,
-                None => ScoreType::ThreeOfAKind,
-            },
-            2 => match counts.values().duplicates().find(|&&count| count == 2) {
-                Some(_i) => ScoreType::TwoPair,
-                None => ScoreType::OnePair
-            },
-            1 => ScoreType::HighCard,
-            _ => panic!("{:?} is not a valid value to compute score on!", counts.values()),
-        };
+            .parse()
+            .unwrap();
         
-        let hand = Hand {
-            card1: faces[0],
-            card2: faces[1],
-            card3: faces[2],
-            card4: faces[3],
-            card5: faces[4],
-            bid: raw_data.next()
-                .unwrap()
-                .parse()
-                .unwrap(),
-            score: score,
-        };
-        hands.push(hand);
+        // Create a new Hand and push it to the list of all hands
+        hands.push(Hand::new(faces, bid));
     }
 
     // Sort all hands by score type, then by card1, card2...
     // This is managed in the Ord impl for Hand
+    // Then reverse the resulting sorted Vector to get the final multipliers 
     hands.sort();
     hands.reverse();
 
